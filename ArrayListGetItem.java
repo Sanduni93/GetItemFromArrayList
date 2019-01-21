7d8bf0fd508bd7a8025af8e1b57a205c78dc77d7
Sanduni93 <nirodhajayaweera1993@gmail.com>
2019-01-22 00:40:26 +0530

    Here specifies about how to get particular item from the array list

---
+++ ArrayListGetItem.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+class ArrayListGetItem{
+
+public static void main(String args[]){
+
+ArrayList<String> laps = new ArrayList<String> ();
+laps.add("DELL");
+laps.add("Asuz");
+laps.add("Acer");
+laps.add("Asuz");
+
+System.out.println(laps.get(1));
+
+}
+
+}
